f0053870853ae4cd41fe809ace244ed7bf1bd299
Rodrigo <rodrigo.a.a.aba@gmail.com>
2024-01-02 14:54:06 -0300

    used vite to fix path to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/versiculos-vue/", // used to deploy on github pages
   plugins: [
     vue(),
   ],
